2f3f4fe62fe59e597f5b1cacbae14643c73bdeb1
haus2100 <haus.esc@gmail.com>
2022-09-08 00:00:21 +0300

    debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
