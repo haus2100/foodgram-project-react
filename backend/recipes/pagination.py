771fb9cc6d1314a964c7c320dfb7442cfbf1d13e
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-14 00:01:07 +0300

    rv2

--- backend/recipes/pagination.py
+++ backend/recipes/pagination.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
+from foodgram.settings import PS
 
 
 class RecipePagination(PageNumberPagination):
-    page_size = 6
+    page_size = PS
     page_size_query_param = "limit"
