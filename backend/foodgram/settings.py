34876833b5c9a6eedfbf92c5f4762b650abcb90b
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-13 12:22:50 +0300

    rv2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
