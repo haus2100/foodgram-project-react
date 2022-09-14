260194e7293938669402668eaa4226c9f637a865
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-14 04:57:51 +0300

    rv2

--- backend/recipes/utlis.py
+++ backend/recipes/utlis.py
@@ -1,9 +1,9 @@
-def shopping_cart_text(ingredients):
-    shopping_cart = 'Список покупок \n\n'
+def create_shopping_list(ingredients):
+    shopping_list = 'Список покупок \n\n'
     for ingredient in ingredients:
-        shopping_cart += (
+        shopping_list += (
             f"{ingredient['ingredient__name']} "
             f"({ingredient['ingredient__measurement_unit']}) - "
             f"{ingredient['amount__sum']}\n"
         )
-    return shopping_cart
+    return shopping_list
