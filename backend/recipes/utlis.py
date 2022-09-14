e67af6a4a954744520a3c5043233fc8ffc4d4281
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-14 23:01:19 +0300

    Delete utlis.py

--- backend/recipes/utlis.py
+++
@@ -1,9 +0,0 @@
-def create_shopping_list(ingredients):
-    shopping_list = 'Список покупок \n\n'
-    for ingredient in ingredients:
-        shopping_list += (
-            f"{ingredient['ingredient__name']} "
-            f"({ingredient['ingredient__measurement_unit']}) - "
-            f"{ingredient['amount__sum']}\n"
-        )
-    return shopping_list
