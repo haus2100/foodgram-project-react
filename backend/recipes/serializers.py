9927972497185cd50bfd1d927feba1f354e9a693
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-16 00:57:19 +0300

    Update serializers.py

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -143,6 +143,41 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
             "text",
             "cooking_time",
         )
+    def validate(self, data):
+        ingredients = data['ingredients']
+        ingredients_list = []
+        for ingredient in ingredients:
+            ingredient_id = ingredient['id']
+            if ingredient_id in ingredients_list:
+                raise serializers.ValidationError(
+                    {'ingredients': 'Только уникальные ингредиенты'}
+                )
+            ingredients_list.append(ingredient_id)
+            amount = ingredient['amount']
+            if int(amount) <= 0:
+                raise serializers.ValidationError(
+                    {'amount': 'Должен быть хотя-бы один ингредиент'}
+                )
+
+        tags = data['tags']
+        if not tags:
+            raise serializers.ValidationError(
+                {'tags': 'Нужно указать минимум один тег'}
+            )
+        tags_list = []
+        for tag in tags:
+            if tag in tags_list:
+                raise serializers.ValidationError(
+                    {'tags': 'Теги должны быть уникальны'}
+                )
+            tags_list.append(tag)
+
+        cooking_time = data['cooking_time']
+        if int(cooking_time) <= 0:
+            raise serializers.ValidationError(
+                {'cooking_time': 'Время приготовление больше 0'}
+            )
+        return data
 
     def _add_tags_and_ingredients(self, recipe, tags_data, ingredients_data):
         recipe.tags.set(tags_data)
