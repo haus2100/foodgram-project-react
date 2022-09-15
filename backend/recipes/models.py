fbf599337531ea437b2228b0633385dc7901f63a
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-15 16:50:29 +0300

    rv3

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -69,10 +69,7 @@ class IngredientAmount(models.Model):
         on_delete=models.CASCADE,
         verbose_name="Ингредиент",
     )
-    amount = models.PositiveIntegerField(
-        validators=[MinValueValidator(1)],
-        verbose_name="Количество"
-    )
+    amount = models.PositiveIntegerField(verbose_name="Количество")
 
     class Meta:
         verbose_name = "Количество ингредиента"
