1aa60e6b6412deffc02705caf29d5743f65f8662
haus2100 <91982697+haus2100@users.noreply.github.com>
2022-09-15 23:46:46 +0300

    Update serializers.py

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,5 +1,6 @@
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from users.serializers import CustomUserSerializer
 
